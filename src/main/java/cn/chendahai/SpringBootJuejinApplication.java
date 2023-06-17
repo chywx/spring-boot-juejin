848a2954ee16279488273639d8f82d7880655e4d
chenhaiyang <chenhaiyang@luckybus.me>
2023-06-17 18:55:35 +0800

    初始化springboot，版本为2.1.9.RELEASE

---
+++ src/main/java/cn/chendahai/SpringBootJuejinApplication.java
@@ -0,0 +1,13 @@
+package cn.chendahai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJuejinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJuejinApplication.class, args);
+    }
+
+}
